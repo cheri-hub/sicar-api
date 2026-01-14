deadb05931fe103a93aaee937f63e2822d59c102
Luis Montagner <luis.moontagner@gmail.com>
2026-01-14 20:30:31 -0300

    fix: ensure LF line endings for shell scripts

--- app/frontend/docker-entrypoint.sh
+++ app/frontend/docker-entrypoint.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+﻿#!/bin/sh
 set -e
 
+# Definir valor padrão se API_KEY não estiver definida
+export API_KEY="${API_KEY:-}"
+
 # Substituir apenas a variável API_KEY no template
-# Usa $$ para escapar variáveis que o nginx deve interpretar
 envsubst '${API_KEY}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Executar o comando passado (nginx)
