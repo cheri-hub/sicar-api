a56a65900697c3ff784273d0a805bc2d5ef91d50
Luis Montagner <luis.moontagner@gmail.com>
2026-01-15 18:08:07 -0300

    fix: convert docker-entrypoint.sh to Unix LF line endings

--- app/frontend/docker-entrypoint.sh
+++ app/frontend/docker-entrypoint.sh
@@ -1,10 +1,10 @@
-﻿#!/bin/sh
+#!/bin/sh
 set -e
 
-# Definir valor padrão se API_KEY não estiver definida
+# Definir valor padrao se API_KEY nao estiver definida
 export API_KEY="${API_KEY:-}"
 
-# Substituir apenas a variável API_KEY no template
+# Substituir apenas a variavel API_KEY no template
 envsubst '${API_KEY}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Executar o comando passado (nginx)
